aa05796b5dae47443919e21a210c6d9ae4da67c3
LuisSilvah <Luisgsilva21@gmail.com>
2024-04-06 19:00:58 -0300

    Formatando tabela | Alterando nome de variveis | Corrindo calculo

--- projeto_integrador.py
+++ projeto_integrador.py
@@ -1,54 +1,64 @@
-print(30*'=')
-print('     Sistema de Cadastro')
-print(30*'=')
+print(36 * "=")
+print("\t Sistema de Cadastro")
+print(36 * "=")
 
-cod_prd1 = input('Digite o código do produto: ')
-name_prd1 = input('Digite o nome do produto: ')
-dresc_prd1 = input('Adicione uma descrição ao produto: ')
-ca_prd1 = float(input('Qual o custo do Produto: '))
-cf = float(input('Qual os custo fixos/administrativos do comércio [%]: '))
-cv = float(input('Qual a comissão de venda do produto,em porcentagem [%]: '))
-iv = float(input('Qual a aliquota de imposto desejada [%]: '))
-ml = float(input('Qual a margem de lucro desejada [%]: '))
+codProduto = input("Digite o código do produto: ")
+nomeProduto = input("Digite o nome do produto: ")
+descProduto = input("Adicione uma descrição ao produto: ")
 
-print('Dados Cadastrados com sucesso!')
+custoProduto = float(input("Qual o custo do Produto: "))
+custoFixoPct = float(input("Qual os custo fixos/administrativos do comércio [%]: "))
+comissaoVendaPct = float(input("Qual a comissão de venda do produto,em porcentagem [%]: "))
+impostoVendaPct = float(input("Qual a aliquota de imposto desejada [%]: "))
+margemLucroPct = float(input("Qual a margem de lucro desejada [%]: "))
 
-pv = ca_prd1/(1-((cf+cv+iv+ml)/100))
-rb = pv - ca_prd1
-oc = (ca_prd1)*(cf+cv+iv)/100 #ARRUMAR FORMULA
-rent = rb-oc
-rent_pct = rent*100/pv
+print("Dados Cadastrados com sucesso!")
 
-cvendas = cv*pv/100
-cfixo = cf*pv/100
-pct_preço_venda = 100*pv/pv
-pct_rb = 100*rb/pv
-pct_oc = 100*oc/pv
+# Preço de venda produto
+precoVenda = custoProduto / (
+    1 - ((custoFixoPct + comissaoVendaPct + impostoVendaPct + margemLucroPct) / 100)
+)
 
+receitaBruta = precoVenda - custoProduto # Receita Bruta
 
-#TERMINAR FORMATACAO TABELA
-valor = 'Valor'
-pct = '[%]'
-print(60*'=')
-print(f'Descrição {valor:^42} {pct:^12}')
-print(60*'-')
-print(f'Preço de Venda: {pv:^30.2f} {pct_preço_venda:^24.0f}')
-print(f'Receita Bruta: {rb:^32} {pct_rb:^21}')
-print(f'Custo Fixo/Administrativo: {cfixo:6} {cf:^49}')
-print(f'Comissão de Vendas: {cvendas:^23} {cv:^29}')
-print(f'Outros Custos: {oc:^32} {pct_oc:^20}')
-print(f'Rentabilidade: {rent:^32} {rent_pct:^20}')
-print(60*'=')
+comissaoVenda = comissaoVendaPct * precoVenda / 100 # Comissão de vendas 
+custoFixo = custoFixoPct * precoVenda / 100 # Custo fixo 
+impostoVenda = impostoVendaPct * precoVenda / 100 # Imposto
 
-if rent_pct>0.1:
-    if rent_pct>0.2:
-        print('Lucro: Alto')
+outrosCustos = custoFixo + comissaoVenda + impostoVenda # Outros custos
+
+rentabilidade = receitaBruta - outrosCustos # Rentabilidade
+
+precoVendaPct = 100 * precoVenda / precoVenda # Preço de venda  %
+custoProdutoPct = custoProduto * 100 / precoVenda # Custo produto %
+receitaBrutaPct = 100 * receitaBruta / precoVenda # Receita bruta  %
+outrosCustosPct = 100 * outrosCustos / precoVenda # Outros custos %
+
+rentabilidadePct = rentabilidade * 100 / precoVenda # Rentabilidade %
+
+# Tabela
+print(64 * "=")
+print(f"Descrição {"Valor":^51} {"[%]":^2}")
+print(64 * "-")
+print(f"Preço de Venda: {precoVenda:^40.2f} {precoVendaPct:7.0f}") 
+print(f"Custo de Aquisição (Fornecedor): {custoProduto:^2.2f} {custoProdutoPct:^48.0f}")
+print(f"Receita Bruta: {receitaBruta:^42.2f} {receitaBrutaPct:^11.0f}") 
+print(f"Custo Fixo/Administrativo: {custoFixo:11.2f} {custoFixoPct:^48.0f}") 
+print(f"Comissão de Vendas: {comissaoVenda:^31.2f} {comissaoVendaPct:^24.0f}") 
+print(f"Imposto: {impostoVenda:^53.2f}{impostoVendaPct:.0f}") 
+print(f"Outros Custos: {outrosCustos:^42.2f} {outrosCustosPct:^10.0f}") 
+print(f"Rentabilidade: {rentabilidade:^42.2f} {rentabilidadePct:^10.0f}") 
+print(64 * "=")
+
+if rentabilidadePct > 10:
+    if rentabilidadePct > 20:
+        print("Lucro: Alto")
     else:
-        print('Lucro: Médio')
-elif rent_pct == 0:
-    print('Lucro: Equilíbrio')
-elif rent_pct < 0 :
-    print('Lucro: Prejuízo')
+        print("Lucro: Médio")
+elif rentabilidadePct == 0:
+    print("Lucro: Equilíbrio")
+elif rentabilidadePct < 0:
+    print("Lucro: Prejuízo")
 else:
-    print('Lucro: Baixo')
-print(60*'=')
+    print("Lucro: Baixo")
+print(64 * "=")
